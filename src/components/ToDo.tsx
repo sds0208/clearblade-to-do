0ea3628e49dbdf7bdbf32b69c8e335ec1be7bd23
sds0208 <sarahsantacruz86@gmail.com>
2018-05-12 18:50:05 -0500

    first commit

---
+++ src/components/ToDo.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class ToDo extends React.Component {
+  public render() {
+    return (
+      <div className="ToDo">
+        To-do rows will go here.
+      </div>
+    );
+  }
+}
+
+export default ToDo;
